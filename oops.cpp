f8a3ac40efcf67c5c14730c6084d6a1fa1edcb0e
Anirban Das <anirbandas0206@gmail.com>
2020-10-15 13:18:49 +0530

    Add files via upload
    Added oops file.

---
+++ oops.cpp
@@ -0,0 +1,46 @@
+
+
+OOPS -->
+1) Polymorphism
+     - - - - -  -
+     a) Runtime Polymorphism - Function overriding
+     b) Compile time Polymorphism - Function overloading, operator overloading
+
+2) Encaplution
+    Wrapping of data and functions in a single using is called encaptulation
+
+3) Inheritance
+        
+
+
+
+4) Abstraction
+        The way of showing the essential features without showing the background complexity.
+
+
+
+class - prototype of an object .. or blue print of an object 
+
+class car
+{
+     private:
+        int a,b,c;
+     protected:
+         
+     public:
+       int aka()
+       {
+       	  return  a;
+       }  
+};
+
+class maruti : public a , public b{
+    
+     public: 
+     	int a , b, c;
+     protected:
+      
+
+
+};
+
